3dc1ac4063ab94e07a9335afd529c153f1b77c00
Zaid <zaid43519@gmail.com>
2026-09-19 00:07:13 +0100

    Set up PAI development environment

---
+++ environment_test.py
@@ -0,0 +1,11 @@
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+print("Python version:")
+print(sys.version)
+print("\nNumPy version:")
+print(np.__version__)
+print("\nPandas version:")
+print(pd.__version__)
+print("\nEnvironment successfully configured!")
---
+++ first_analysis.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+data = {
+"City": ["Dublin", "Cork", "Galway", "Limerick"],
+"Value": [42, 31, 27, 22]
+}
+df = pd.DataFrame(data)
+print(df)
+df.plot(
+x="City",
+y="Value",
+kind="bar",
+legend=False,
+title="Example PAI Visualisation"
+)
+plt.ylabel("Value")
+plt.tight_layout()
+plt.show()
